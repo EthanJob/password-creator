c8dd3312d315ecd732e392910d456ab569f07f9f
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-10-23 13:10:00 -0400

    testing array functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+
+let currentPasswords = [];
+
+const addPassword = () => {
+  newPassword = "redbull";
+  newPassword1 = "monster";
+  currentPasswords.push(newPassword, newPassword1);
+}
+
+addPassword();
+
+console.log(currentPasswords);
 console.log("app.js is connected");
