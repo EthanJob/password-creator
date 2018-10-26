1f83c94f7c6dc987ac5b4c2d5d7a515e6100e0cc
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-10-26 18:55:59 -0400

    cleaned up code

--- js/app.js
+++ js/app.js
@@ -2,15 +2,6 @@ const app = angular.module('PasswordApp', []);
 
 app.controller('MainController', function($scope) {
 
-  this.currentPasswords = [];
-
-  // this.addPassword = () => {
-  //   newPassword = "redbull";
-  //   newPassword1 = "monster";
-  //   this.currentPasswords.push(newPassword, newPassword1);
-  // }
-  // this.addPassword();
-
   this.password = '';
 
   this.characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -26,7 +17,6 @@ this.run = () => {
 
 // this.run();
 
-  // console.log(this.currentPasswords);
   console.log("app.js is connected");
 
 });
