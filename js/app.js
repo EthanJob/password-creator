967d36912fb393f19d484abc7ea19ea10e07f210
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-10-26 18:53:25 -0400

    displaying password on page

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-// const app = angular.module('PasswordApp', []);
-//
-// app.controller('MainController', function($scope) {
-//
-//   this.currentPasswords = [];
+const app = angular.module('PasswordApp', []);
+
+app.controller('MainController', function($scope) {
+
+  this.currentPasswords = [];
 
   // this.addPassword = () => {
   //   newPassword = "redbull";
@@ -16,6 +16,7 @@
   this.characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 this.run = () => {
+  this.password = '';
   for (var i = 0; i < 10; i++) {
     this.item = Math.floor(Math.random() * this.characters.length);
     this.password += this.characters[this.item];
@@ -23,9 +24,9 @@ this.run = () => {
   console.log('New password: ', this.password);
 };
 
-this.run();
+// this.run();
 
   // console.log(this.currentPasswords);
-  // console.log("app.js is connected");
+  console.log("app.js is connected");
 
-// });
+});
