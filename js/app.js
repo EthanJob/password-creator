207e189451ef3476f8caaabbfb4a6924808277e3
EthanJob <ethanlouis_fj@yahoo.com>
2019-08-20 11:47:59 -0400

    added special characters

--- js/app.js
+++ js/app.js
@@ -6,7 +6,12 @@ app.controller('MainController', function($scope) {
 
   this.password = '';
 
-  this.characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  this.characters = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'D', 'E', 'F',
+  'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't',
+   'u', 'v', 'w', 'x', 'y', 'z', '!', '@', '#', '$', '*', '?'
+];
 
 this.run = () => {
   this.password = '';
